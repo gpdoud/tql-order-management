901bef5a7b54c64c739a95aabf0c035761093b41
Greg Doud <gpdoud@gmail.com>
2021-06-25 14:07:45 -0400

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CustomerCreateComponent } from './customer/customer-create/customer-create.component';
+import { CustomerDetailComponent } from './customer/customer-detail/customer-detail.component';
+import { CustomerEditComponent } from './customer/customer-edit/customer-edit.component';
+import { CustomerListComponent } from './customer/customer-list/customer-list.component';
+
+import { E404Component } from './misc/e404/e404.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/cust/list', pathMatch: 'full' },
+  { path: 'cust/list', component: CustomerListComponent },
+  { path: 'cust/detail/:id', component: CustomerDetailComponent },
+  { path: 'cust/create', component: CustomerCreateComponent },
+  { path: 'cust/edit/:id', component: CustomerEditComponent },
+  { path: '**', component: E404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
